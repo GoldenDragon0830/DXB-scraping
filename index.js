00da169812a7b72be6646a759db505179d9b3ff2
goldendragon0830 <goldendragon0830@gmail.com>
2025-03-26 17:47:39 -0400

    1

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const puppeteer = require('puppeteer');
 
+async function waitForTimeout(timeout) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, timeout)
+    })
+}
+
 async function scrapeTransactionDetails(url) {
     const browser = await puppeteer.launch({
         headless: false, 
@@ -18,7 +24,7 @@ async function scrapeTransactionDetails(url) {
         await page.waitForSelector('#SearchInput');
         await page.type('#SearchInput', 'Jumeirah Lakes Towers');
 
-        await page.waitForTimeout(3000);
+        await waitForTimeout(3000);
   
         // await page.screenshot({ path: 'debug.png', fullPage: true });
 
@@ -28,7 +34,7 @@ async function scrapeTransactionDetails(url) {
         await page.waitForSelector('.submit-btn');
         await page.click('.submit-btn');
 
-        await page.waitForTimeout(10000);
+        await waitForTimeout(10000);
     } catch (error) {
         console.error('Error during scraping:', error);
     } finally {
