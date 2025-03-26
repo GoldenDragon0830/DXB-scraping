9ae8f2c81aaa2c77aacebbe30f9ee14fd36e2ebe
goldendragon0830 <goldendragon0830@gmail.com>
2025-03-26 18:19:57 -0400

    1

--- index.js
+++ index.js
@@ -20,24 +20,26 @@ async function scrapeTransactionDetails(url) {
             waitUntil: 'load', // Wait until the network is idle
             timeout: 60000, // 60 seconds timeout
         });
-
+        console.log('@@@@@@@@@@@@@');
         // await waitForTimeout(3000);
-
-        await page.waitForSelector('#FeaturedSearches > article:nth-child(1)', {visible: true, timeout: 10000});
+        await page.waitForSelector("#134_menubar_1i", {timeout: 30000});
+        await page.click('#134_menubar_1i');
         
-        await page.waitForSelector('#SearchInput');
-        await page.click('#SearchInput');
-        await page.type('#SearchInput', 'Jumeirah Lakes Towers');
+        // await page.waitForSelector('#FeaturedSearches > article:nth-child(1)', {timeout: 30000});
+        
+        // await page.waitForSelector('#SearchInput');
+        // await page.click('#SearchInput');
+        // await page.type('#SearchInput', 'Jumeirah Lakes Towers');
   
-        // await page.screenshot({ path: 'debug.png', fullPage: true });
+        // // await page.screenshot({ path: 'debug.png', fullPage: true });
 
-        await page.waitForSelector('#AreasList > article:first-of-type', { visible: true, timeout: 10000 });
-        await page.click('#AreasList > article:first-of-type');
+        // await page.waitForSelector('#AreasList > article:first-of-type', { visible: true, timeout: 30000 });
+        // await page.click('#AreasList > article:first-of-type');
 
-        await page.waitForSelector('.submit-btn');
-        await page.click('.submit-btn');
+        // await page.waitForSelector('.submit-btn');
+        // await page.click('.submit-btn');
 
-        await waitForTimeout(10000);
+        // await waitForTimeout(10000);
     } catch (error) {
         console.error('Error during scraping:', error);
     } finally {
