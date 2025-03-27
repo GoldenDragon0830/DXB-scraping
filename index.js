29b051d7843b9f11e184bb0400b8cf22367d1b7c
goldendragon0830 <goldendragon0830@gmail.com>
2025-03-26 21:09:23 -0400

    123

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ puppeteer.use(StealthPlugin());
 
 async function scrapeTransactionDetails(url) {
 
-    // const proxyServer = '137.59.4.211:6080';
+    const proxyServer = '137.59.4.211:6080';
 
     const browser = await puppeteer.launch({
         headless: false, 
         defaultViewport: null,
         args: [
             '--start-maximized',
-            // `--proxy-server=${proxyServer}`
+            `--proxy-server=${proxyServer}`
         ]
     });
 
@@ -25,30 +25,25 @@ async function scrapeTransactionDetails(url) {
             'accept-language': 'en-US,en;q=0.9',
         });
 
-        // await page.authenticate({
-        //     username: 'PICpic123',
-        //     password: 'upwork123'
-        // });
+        await page.authenticate({
+            username: 'PICpic123',
+            password: 'upwork123'
+        });
 
         await page.goto(url, {
             waitUntil: 'load', // Wait until the network is idle
             timeout: 60000, // 60 seconds timeout
         });
-
-        // await waitForTimeout(3000);
-        // await page.waitForSelector("#134_menubar_1i", {timeout: 30000});
-        // await page.click('#134_menubar_1i');
         
-        // await page.waitForSelector('#FeaturedSearches > article:nth-child(1)', {timeout: 30000});
-        
-        // await page.waitForSelector('#SearchInput');
-        // await page.click('#SearchInput');
-        // await page.type('#SearchInput', 'Jumeirah Lakes Towers');
-  
-        // // await page.screenshot({ path: 'debug.png', fullPage: true });
+        await page.waitForSelector("#LocationsStack > a", {timeout: 30000});
+        await page.click('#LocationsStack > a');
 
-        // await page.waitForSelector('#AreasList > article:first-of-type', { visible: true, timeout: 30000 });
-        // await page.click('#AreasList > article:first-of-type');
+        await page.waitForSelector("#SearchInput", {timeout: 30000});
+        await page.click('#SearchInput');
+        await page.type('#SearchInput', 'Jumeirah Lakes Towers');
+        
+        await page.waitForSelector('#AreasList > article:first-of-type', { visible: true, timeout: 30000 });
+        await page.click('#AreasList > article:first-of-type');
 
         // await page.waitForSelector('.submit-btn');
         // await page.click('.submit-btn');
