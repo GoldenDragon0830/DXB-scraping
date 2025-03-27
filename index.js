ac1a7f05a8fc739c30c52431d13bb7e960f45792
goldendragon0830 <goldendragon0830@gmail.com>
2025-03-26 21:15:24 -0400

    123

--- index.js
+++ index.js
@@ -46,12 +46,12 @@ async function scrapeTransactionDetails(url) {
         await page.waitForSelector("#LocationsStack > a", {timeout: 30000});
         await page.click('#LocationsStack > a');
 
-        // await page.waitForSelector("#SearchInput", {timeout: 30000});
-        // await page.click('#SearchInput');
-        // await page.type('#SearchInput', 'Jumeirah Lakes Towers');
+        await page.waitForSelector("#SearchInput", {timeout: 30000});
+        await page.click('#SearchInput');
+        await page.type('#SearchInput', 'Jumeirah Lakes Towers');
         
-        // await page.waitForSelector('#AreasList > article:first-of-type', { timeout: 30000 });
-        // await page.click('#AreasList > article:first-of-type');
+        await page.waitForSelector('#AreasList > article:first-of-type', { timeout: 30000 });
+        await page.click('#AreasList > article:first-of-type');
 
         // await page.waitForSelector('.submit-btn');
         // await page.click('.submit-btn');
