5f84c6476a27268f17ebbb18411d2054592f64eb
goldendragon0830 <goldendragon0830@gmail.com>
2025-03-27 04:09:02 -0400

    1

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ async function scrapeTransactionDetails(url) {
                 for(let i=0; i<detailButtons.length ; i++) {
                     await detailButtons[i].click();
                     await waitForTimeout(2000);
-
+                    console.log(i+1);
                     await page.waitForSelector("#t_PageBody > div.ui-dialog.ui-corner-all.ui-widget.ui-widget-content.ui-front.ui-dialog--apex.t-Dialog-page--standard.ui-draggable > div.ui-dialog-titlebar.ui-corner-all.ui-widget-header.ui-helper-clearfix.ui-draggable-handle > button", { timeout: 30000 });
                     await page.click("#t_PageBody > div.ui-dialog.ui-corner-all.ui-widget.ui-widget-content.ui-front.ui-dialog--apex.t-Dialog-page--standard.ui-draggable > div.ui-dialog-titlebar.ui-corner-all.ui-widget-header.ui-helper-clearfix.ui-draggable-handle > button");
                 }
