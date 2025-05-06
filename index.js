6cb692a0439ea517973a7028920909595869a4b2
goldendragon0830 <goldendragon0830@gmail.com>
2025-05-06 03:27:35 -0400

    1

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ async function scrapeTransactionDetails(url) {
             await waitForTimeout(2000);
             
             await page.waitForSelector('#R7990151324798006877 > div > ul > li:nth-child(2)', { timeout: 30000 });
-            await page.click('#R7990151324798006877 > div > ul > li:nth-child(7)');
+            await page.click('#R7990151324798006877 > div > ul > li:nth-child(2)');
     
             await waitForTimeout(2000);
     
