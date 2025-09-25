f20292de495f315e9d4746abc24a84e5e536415b
goldendragon0830 <goldendragon0830@gmail.com>
2025-09-25 05:49:54 -0400

    1

--- index.js
+++ index.js
@@ -136,11 +136,13 @@ async function scrapeTransactionDetails(url, property_id, project_name, building
 
             await page.waitForSelector("#report_7995429268703774766_catch > ul > li:nth-child(1) > span > span.t-BadgeList-value > div > span:nth-child(2)", { timeout: 30000 });
             
-            const median_price = await document.querySelector("#report_7995429268703774766_catch > ul > li:nth-child(1) > span > span.t-BadgeList-value > div > span:nth-child(2)").textContent.trim();
+            const median_price = await page.evaluate(() => {
+                return document.querySelector("#report_7995429268703774766_catch > ul > li:nth-child(1) > span > span.t-BadgeList-value > div > span:nth-child(2)").textContent.trim();
+            });
             
-            console.log(median_price)
+            console.log(median_price);
 
-            return;
+            return { median_price };
         }
 
         await page.evaluate(() => {
